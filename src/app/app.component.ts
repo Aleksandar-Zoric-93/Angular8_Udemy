3e7724c8463053d2b78b47a620faaaa8cda1faa4
Aleksandar Zoric <aleksandarzoric93@gmail.com>
2020-03-01 14:20:39 +0000

    ngFor, ngStyle and ngClass assignment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,11 @@ import { Component } from '@angular/core';
   }`]
 })
 export class AppComponent {
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
