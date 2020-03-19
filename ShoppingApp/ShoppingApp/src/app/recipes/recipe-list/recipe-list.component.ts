65009b7139a51a2364e78a64280112615a971e48
Aleksandar Zoric <aleksandarzoric93@gmail.com>
2020-03-19 15:12:20 +0000

    Outputting list of recipes with ngFor.

--- ShoppingApp/ShoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShoppingApp/ShoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Recipe name here', 'Recipe Description', 'https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg')
+    new Recipe('Recipe name here', 'Recipe Description', 'https://www.cscassets.com/recipes/wide_cknew/wide_32.jpg'),
+    new Recipe('Recipe name here 2', 'Recipe Description 2', 'https://i.pinimg.com/originals/1a/f4/67/1af467d05e95579ea4f46734182ef13f.jpg')
   ];
   constructor() { }
 
